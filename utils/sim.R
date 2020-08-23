8c30dbbfb803e13383792d80061ff91365d87bbc
Lu Zhang <lu.zhang@ucla.edu>
2020-08-22 18:33:08 -0700

    move sim to folder utils

--- utils/sim.R
+++ utils/sim.R
@@ -146,38 +146,3 @@ find_typical <- function(model, data, param_path, M = 100) {
 # data = list(D = D)
 # opath <- opt_path_stan(model, data = data, N = 75, init_bound = 5)
 # find_typical(model, data, opath)
-
-
-### functions for obtaining posterior interval of models in posteriordb
-lp_Int_q_posteriordb <- function(pick_model, alpha){
-  
-  ###
-  #' Obtain the lower and upper bound of log density posterior interval of the picked model
-  #' alpha: return the bounds of (1.0 - alpha) * 100% posterior interval
-  #' 
-  ###
-  
-  sc <- stan_code(pick_model)
-  model <- stan_model(model_code = sc)
-  data <- get_data(pick_model)
-  pos_draws <- reference_posterior_draws(po, type = "draws")
-  lp_recovers <- lp_recover(model, data, pos_draws)
-  quantile(lp_recovers, probs = c(alpha / 2.0, 1.0 - alpha / 2.0))
-  
-}
-
-lp_recover <- function(model, data, pos_draws){
-  
-  ###
-  #' recover the log density based on the posterior draws
-  #' 
-  ###
-  
-  posterior <- to_posterior(model, data)
-  fn <- function(theta){
-    log_prob(posterior, unconstrain_pars(posterior, theta), 
-             adjust_transform = TRUE, gradient = TRUE)[1]
-  }
-  lpn <- function(gsd_l) apply(sapply(gsd_l, unlist), 1, fn)
-  sapply(pos_draws, lpn)
-}
