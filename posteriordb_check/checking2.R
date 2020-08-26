a0db7a60a8e43e90e4a455654af552d69f981571
Lu Zhang <lu.zhang@ucla.edu>
2020-08-25 18:56:16 -0700

    Update checking2

--- posteriordb_check/checking2.R
+++ posteriordb_check/checking2.R
@@ -22,20 +22,17 @@ alpha = 0.01
 L = 1000
 M = 20
 width = 860; height = 740 # the size of the plot
-mc.cores = parallel::detectCores() - 2
-sample_seed = 1234
 
 takeoff <- c(21, 24)   # model 21, 24 has transformed pars
 load("../results/lp_posteriordb_explore.RData")
 
-## A table of output per program with median and min and max ##
+## find the model with fitting results
 N_models = 0
 model_record = c()
 for(l in 1:L_pn){
   modelname <- pn[l]
   # pick model
   po <- posterior(modelname, pdb = pd)
-  # get reference posterior samples
   skip_to_next <- FALSE
   tryCatch(gsd <- reference_posterior_draws(po),
            error = function(e) { skip_to_next <<- TRUE})
@@ -45,17 +42,18 @@ for(l in 1:L_pn){
     next }
   N_models = N_models + 1
   model_record = c(model_record, l)
-  #printf("model %d: %s", l, modelname)
 }
 N_models
 
 df <- data.frame(n_iters = c(lp_explore_n_iters[, model_record]), 
-                 n_leapfrogs = c(lp_explore_n_leapfrog[, model_record]),
+                 n_leapfrogs = 
+                   c(lp_explore_n_leapfrog[, model_record]),
                  model = rep(pn[model_record], each = M))
 
 jpeg(filename = paste0("../pics/box_iters_log.jpeg"),
      width = width*1.3, height = height*2, units = "px", pointsize = 12)
-p_box_iter <- ggplot(df, aes(y = model, x = n_iters)) + geom_boxplot() + 
+p_box_iter <- ggplot(df, aes(y = reorder(model, n_iters, FUN = median), 
+                             x = n_iters)) + geom_boxplot() + 
   scale_x_log10() + ylab("") + xlab("No. of iterations") + 
   theme_grey(base_size = 26)
 print(p_box_iter)
@@ -63,7 +61,8 @@ dev.off()
 
 jpeg(filename = paste0("../pics/box_leapfrogs_log.jpeg"),
      width = width*1.3, height = height*2, units = "px", pointsize = 12)
-p_box_leapfrog <- ggplot(df, aes(y = model, x = n_leapfrogs)) + 
+p_box_leapfrog <- ggplot(df, aes(y = reorder(model, n_leapfrogs, FUN = median), 
+                                 x = n_leapfrogs)) + 
   geom_boxplot() + scale_x_log10() + ylab("") + xlab("No. of leapfrogs") + 
   theme_grey(base_size = 26)
 p_box_leapfrog
