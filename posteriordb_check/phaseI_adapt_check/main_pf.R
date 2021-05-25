6ea1afce64d2512e3f60e74514144bc4d6379d46
Lu Zhang <lu.zhang@ucla.edu>
2021-05-24 23:57:44 -0400

    update main_pf

--- posteriordb_check/phaseI_adapt_check/main_pf.R
+++ posteriordb_check/phaseI_adapt_check/main_pf.R
@@ -38,9 +38,9 @@ if(sen_test_L){
   factr_tol = 1e7 # relative tolerance = 1-4 is not enough, should use at least 1e7
 }
 
-# N_sam_DIV #
+# larger sample size for ELBO evaluation #
 sen_test_K <- FALSE
-if(FALSE){
+if(sen_test_K){
   N_sam_DIV = 30  # samples for ELBO evaluation
 }
 
@@ -51,7 +51,7 @@ if(sen_test_J){
 }
 
 # load precalculated results #
-load(file = "../results/lp_posteriordb_LBFGS_h10.RData")
+load(file = "../results/lp_posteriordb_LBFGS_h6.RData")
 set.seed(123)
 
 # preallocate results #
@@ -72,8 +72,6 @@ for(i in 1:49){ #length(model_record)
   sc <- stan_code(po)
   model <- stan_model(model_code = sc)
   
-  # file <- paste0("./posterior_database/models/stan/accel_gp_3.stan")
-  # model <- stan_model(file)
   ###  get the data  ###
   data <- get_data(po)
   
@@ -89,24 +87,74 @@ for(i in 1:49){ #length(model_record)
   print(proc.time() - t)
   # opath <- lp_opath[[i]]$opath
   
-  pick_samples <- Imp_Resam_Each(opath, seed = 1)
-  # pick_samples <- Imp_Resam_WOR(opath, n_inits = 20, seed = 1)
+  pick_samples <- random_sample_Each(opath, seed = 1) # randomly pick one approximating draws for each run of Pathfinder
+  # pick_samples <- Imp_Resam_Each(opath, seed = 1)   # use PSIS-IR to pick one approximating draws for each run of Pathfinder (no longer used in the paper)
+  # pick_samples <- Imp_Resam_WOR(opath, n_inits = 20, seed = 1) # use PSIS-IR to pick n_inits distinct approximating draws for multi-path Pathfinder
 
   lp_opath[[i]] <- list(opath = opath, pick_samples = pick_samples)
   
 }
 proc.time() - t_0
 
-save(file = "../results/lp_posteriordb_phI_adapt_default.RData",
+save(file = "../results/lp_posteriordb_phI_adapt_long_hist.RData",
      list = c("lp_opath"))
 # _default
+# _short_L
+# _large_K
 # _long_hist
 
 lapply(lp_opath, f <- function(x){ncol(x$pick_samples)})
 
 
+## multi-path Pathfinder ##
+# preallocate results #
+lp_multi_opath <- c()
+
+t_0 <- proc.time()
+for(i in 1:49){ #length(model_record)
+  modelname <- pn[model_record[i]]
+  printf("model %d: %s", model_record[i], modelname)
+  
+  # pick model
+  po <- posterior(modelname, pdb = pd)
+  # get reference posterior samples
+  gsd <- reference_posterior_draws(po)
+  
+  # compile the model
+  sc <- stan_code(po)
+  model <- stan_model(model_code = sc)
+
+  ###  get the data  ###
+  data <- get_data(po)
+  
+  # set up lmm #
+  posterior <- to_posterior(model, data)
+  D <- get_num_upars(posterior)
+  cat("No. pars:", D," lmm in L-BFGS: ", lmm, "\n")
+  
+  lp_multi_opath[[i]] <- list()
+  t <- proc.time()
+  for(j in 1:100){
+    cat(j, "\t")
+    seed_list = (j-1)*20 + 1:20
+    opath <- opt_path_stan_parallel(seed_list, seed_list, mc.cores, model, data,
+                                    init_bound = init_bound, N1, N_sam_DIV, N_sam, 
+                                    factr_tol, lmm)
+    pick_samples <- Imp_Resam_WOR(opath, n_inits = N_sam, seed = 1)
+    lp_multi_opath[[i]][[j]] <- pick_samples   # record the 100 samples 
+  }
+  print(proc.time() - t)
+  
+}
+proc.time() - t_0
+
+save(file = "../results/multi_pf_samples_default.RData",
+     list = c("lp_multi_opath"))
+
+
+############### old code #######################
 # random sample 4 pathfinder + SIR WOR #
-load(file = "../results/lp_posteriordb_LBFGS_h10.RData")
+load(file = "../results/lp_posteriordb_LBFGS_h6.RData")
 load("../results/lp_posteriordb_phI_adapt_large_K.RData")
 set.seed(123)
 
