a94fb982fa997342549b2a8ac6c9e13463bce48f
Lu Zhang <lu.zhang@ucla.edu>
2021-08-04 15:49:28 -0400

    update the simulation for ADVI with corrected calc_log_g

--- posteriordb_check/phaseI_ADVI/main_ADVI_100.R
+++ posteriordb_check/phaseI_ADVI/main_ADVI_100.R
@@ -10,7 +10,7 @@ library(posteriordb)
 library(posterior)
 library(ggplot2)
 library(cmdstanr)
-set_cmdstan_path("/home/luzhang/Google Drive/Github/adaptation/cmdstan-lowrank_robust")
+set_cmdstan_path("/home/luzhang/Google Drive/Github/adaptation/cmdstan-2.26.1") # specify the path of cmdstan 
 library(loo)
 source("../utils/sim_pf.R")
 source("../utils/lp_utils.R")
@@ -35,11 +35,14 @@ ADVI_meanfield_draw <- list()
 ADVI_meanfield_center <- list() 
 ADVI_meanfield_draw_100 <- list()
 ADVI_meanfield_lrs_100 <- list()
-iter_fit_mean <- array(data = 0, dim = c(M, length(model_record)))
 calls_lp_mean <- array(data = 0, dim = c(M, length(model_record)))
 calls_gr_mean <- array(data = 0, dim = c(M, length(model_record)))
 
 eta_sequence <- c(100, 10, 1, 0.1, 0.01)
+
+ELBO_ADVI_mf = array(data = NA, dim = c(M, length(model_record)))
+iter_ADVI_mf = array(data = NA, dim = c(M, length(model_record)))
+
 ## check meanfield ##
 for(i in 1:length(model_record)){
   modelname <- pn[model_record[i]]
@@ -61,10 +64,13 @@ for(i in 1:length(model_record)){
   cat("benchmark lp INV:", round(lp_INV[, i], digits = 3), 
       "benchmark lp_ mean:", round(lp_mean[i], digits = 3), "\n")
   
+  lp <- function(theta) log_prob(posterior, theta, adjust_transform = TRUE, 
+                                 gradient = TRUE)[1]
+  
   ###  run ADVI through cmdstanr  ###
   # need to run the following line to generate the stan file. no need to run if there is one. 
-  # write_stan_file(sc, dir = paste0(getwd(), "/modelcode"),
-  #                 basename = paste0(modelname, ".stan"))
+  write_stan_file(sc, dir = paste0(getwd(), "/modelcode"),
+                  basename = paste0(modelname, ".stan"))
   file <- file.path(getwd(), "modelcode", paste0(modelname, ".stan"))
   mod <- cmdstan_model(file)
   
@@ -93,13 +99,17 @@ for(i in 1:length(model_record)){
       ADVI_meanfield_center[[i]][, j] <-  colMeans(unconstrained_draws)
       lrs <- c(fit_ADVI$draws("lp__") - fit_ADVI$draws("lp_approx__"))
       ADVI_meanfield_lrs_100[[i]][[j]] <- lrs
-      finit_ind <- which(is.finite(lrs))
-      pick_ind <- sample(finit_ind, size = 1)
+      pick_ind <- sample.int(nrow(unconstrained_draws), size = 1)
       ADVI_meanfield_draw[[i]][, j] <- unconstrained_draws[pick_ind, ]
+      
+      # record ELBO for RVI#
+      lp_recal <- apply(unconstrained_draws, 1, lp)
+      ELBO_ADVI_mf[j, i] = mean(lp_recal - fit_ADVI$lp_approx())
+      
       latent_dyn <- read.csv(fit_ADVI$latent_dynamics_files())
       iter_fit <- as.numeric(latent_dyn$X..stan_version_major...2[
         length(latent_dyn$X..stan_version_major...2)-2])
-      iter_fit_mean[j, i] <- iter_fit
+      iter_ADVI_mf[j, i] <- iter_fit
       eta_i = which(eta_sequence == fit_ADVI$metadata()$eta)
       calls_lp_mean[j, i] <- calls_lp_mean[j, i] + iter_fit + 100 * min(eta_i+2, 6)
       calls_gr_mean[j, i] <- calls_gr_mean[j, i] + iter_fit + 50 * min(eta_i+1, 5)
@@ -160,10 +170,11 @@ for(i in 1:length(model_record)){
 ADVI_fullrank_draw <- list() 
 ADVI_fullrank_draw_100 <- list() 
 ADVI_fullrank_lrs_100 <- list()
-iter_fit_full <- array(data = 0, dim = c(M, length(model_record)))
+ELBO_ADVI_fr = array(data = NA, dim = c(M, length(model_record)))
+iter_ADVI_fr <- array(data = 0, dim = c(M, length(model_record)))
 calls_lp_full <- array(data = 0, dim = c(M, length(model_record)))
 calls_gr_full <- array(data = 0, dim = c(M, length(model_record)))
-for(i in 21:length(model_record)){
+for(i in 1:length(model_record)){
   modelname <- pn[model_record[i]]
   printf("model %d: %s", model_record[i], modelname)
   # pick model
@@ -182,6 +193,9 @@ for(i in 21:length(model_record)){
   
   cat("benchmark lp INV:", round(lp_INV[, i], digits = 3), 
       "benchmark lp_ mean:", round(lp_mean[i], digits = 3), "\n")
+  
+  lp <- function(theta) log_prob(posterior, theta, adjust_transform = TRUE, 
+                                 gradient = TRUE)[1]
   
   ###  run ADVI through cmdstanr  ###
   file <- file.path(getwd(), "modelcode", paste0(modelname, ".stan"))
@@ -210,13 +224,18 @@ for(i in 21:length(model_record)){
       ADVI_fullrank_draw_100[[i]][[j]] <- unconstrained_draws # record the last sample of phase I
       lrs <- c(fit_ADVI$draws("lp__") - fit_ADVI$draws("lp_approx__"))
       ADVI_fullrank_lrs_100[[i]][[j]] <- lrs
-      finit_ind <- which(is.finite(lrs))
-      pick_ind <- sample(finit_ind, size = 1)
+      pick_ind <- sample.int(nrow(unconstrained_draws), size = 1)
       ADVI_fullrank_draw[[i]][, j] <- unconstrained_draws[pick_ind, ]
+      
+      # record ELBO for ADVI#
+      lp_recal <- apply(unconstrained_draws, 1, lp)
+      ELBO_ADVI_fr[j, i] = mean(lp_recal - fit_ADVI$lp_approx())
+      
+      
       latent_dyn <- read.csv(fit_ADVI$latent_dynamics_files())
       iter_fit <- as.numeric(latent_dyn$X..stan_version_major...2[
         length(latent_dyn$X..stan_version_major...2)-2])
-      iter_fit_full[j, i] <- iter_fit
+      iter_ADVI_fr[j, i] <- iter_fit
       eta_i = which(eta_sequence == fit_ADVI$metadata()$eta)
       calls_lp_full[j, i] <- calls_lp_full[j, i] + iter_fit + 100 * min(eta_i+2, 6)
       calls_gr_full[j, i] <- calls_gr_full[j, i] + iter_fit + 50 * min(eta_i+1, 5)
@@ -275,80 +294,82 @@ for(i in 21:length(model_record)){
   }
 }
 
-save(file = "../results/ADVI_100_RVI.RData",
+save(file = "../results/ADVI_100_updat.RData",
      list = c("ADVI_meanfield_draw", "ADVI_meanfield_center", 
               "ADVI_meanfield_draw_100", "ADVI_meanfield_lrs_100",
               "ADVI_fullrank_draw", "ADVI_fullrank_draw_100",
               "ADVI_fullrank_lrs_100",
               "calls_lp_mean", "calls_gr_mean",
-              "calls_lp_full", "calls_gr_full"))
+              "calls_lp_full", "calls_gr_full",
+              "ELBO_ADVI_mf", "ELBO_ADVI_fr",
+              "iter_ADVI_mf", "iter_ADVI_fr"))
 
 
 
 ######### old code ###########
 # randomly sample 10 ADVI + SIR WOR #
-Imp_Resam_WOR_ADVI <- function(ADVI_draws, ADVI_lrs, n_inits, seed = 123){
-  
-  #' SIR without replacement, index of apporoximating distribution as an auxilary variable
-  #' Return n_inits distinct samples 
-  
-  ## extract samples and log ratios ##
-  samples <- do.call("rbind", ADVI_draws)
-  lrms <- c(unlist(ADVI_lrs))
-  
-  ## take off samples with infinite log ratios
-  finit_ind <- is.finite(lrms)
-  samples <- samples[finit_ind, ]
-  lrms <- lrms[finit_ind]
-  
-  ## compute the importance weight ##
-  sample_weights_psis <- suppressWarnings(weights(psis(lrms, r_eff = NA),
-                                                  log = FALSE))
-  sample_weights_IS <- exp(lrms - max(lrms))/sum(exp(lrms - max(lrms)))
-  
-  ## Importance resampling ##
-  set.seed(seed)
-  sample_ind <-sample.int(nrow(samples), size = n_inits, replace = FALSE, 
-                          prob = sample_weights_psis)
-  
-  return(samples[sample_ind, ])
-}
-
-
-MC = 100
-load(file = "../results/ADVI_100.RData")
-set.seed(123)
-
-ADVI_mf_10_draws <- list()
-ADVI_fr_10_draws <- list()
-
-which(model_record == 48)
-t_0 <- proc.time()
-for(i in 1:49){ #length(model_record)
-  modelname <- pn[model_record[i]]
-  printf("model %d: %s", model_record[i], modelname)
-  
-  ADVI_mf_10_draws[[i]] <- list()
-  ADVI_fr_10_draws[[i]] <- list()
-  for(j in 1:MC){
-    set.seed(j)
-    pick_ind <- sample.int(MC, 10, replace = FALSE)
-    ADVI_mf_10_draws[[i]][[j]] <- 
-      Imp_Resam_WOR_ADVI(ADVI_meanfield_draw_100[[i]][pick_ind], 
-                         ADVI_meanfield_lrs_100[[i]][pick_ind], 
-                         n_inits = 10, seed = j)
-    ADVI_fr_10_draws[[i]][[j]] <- 
-      Imp_Resam_WOR_ADVI(ADVI_fullrank_draw_100[[i]][pick_ind], 
-                         ADVI_fullrank_lrs_100[[i]][pick_ind], 
-                         n_inits = 10, seed = j)
-  }
-}
-proc.time() - t_0
-
-save(file = "../results/ADVI_10_draws.RData",
-     list = c("ADVI_mf_10_draws", "ADVI_fr_10_draws"))
-
-
-
-
+#' Imp_Resam_WOR_ADVI <- function(ADVI_draws, ADVI_lrs, n_inits, seed = 123){
+#'   
+#'   #' SIR without replacement, index of apporoximating distribution as an auxilary variable
+#'   #' Return n_inits distinct samples 
+#'   
+#'   ## extract samples and log ratios ##
+#'   samples <- do.call("rbind", ADVI_draws)
+#'   lrms <- c(unlist(ADVI_lrs))
+#'   
+#'   ## take off samples with infinite log ratios
+#'   finit_ind <- is.finite(lrms)
+#'   samples <- samples[finit_ind, ]
+#'   lrms <- lrms[finit_ind]
+#'   
+#'   ## compute the importance weight ##
+#'   sample_weights_psis <- suppressWarnings(weights(psis(lrms, r_eff = NA),
+#'                                                   log = FALSE))
+#'   sample_weights_IS <- exp(lrms - max(lrms))/sum(exp(lrms - max(lrms)))
+#'   
+#'   ## Importance resampling ##
+#'   set.seed(seed)
+#'   sample_ind <-sample.int(nrow(samples), size = n_inits, replace = FALSE, 
+#'                           prob = sample_weights_psis)
+#'   
+#'   return(samples[sample_ind, ])
+#' }
+#' 
+#' 
+#' MC = 100
+#' load(file = "../results/ADVI_100.RData")
+#' set.seed(123)
+#' 
+#' ADVI_mf_10_draws <- list()
+#' ADVI_fr_10_draws <- list()
+#' 
+#' which(model_record == 48)
+#' t_0 <- proc.time()
+#' for(i in 1:49){ #length(model_record)
+#'   modelname <- pn[model_record[i]]
+#'   printf("model %d: %s", model_record[i], modelname)
+#'   
+#'   ADVI_mf_10_draws[[i]] <- list()
+#'   ADVI_fr_10_draws[[i]] <- list()
+#'   for(j in 1:MC){
+#'     set.seed(j)
+#'     pick_ind <- sample.int(MC, 10, replace = FALSE)
+#'     ADVI_mf_10_draws[[i]][[j]] <- 
+#'       Imp_Resam_WOR_ADVI(ADVI_meanfield_draw_100[[i]][pick_ind], 
+#'                          ADVI_meanfield_lrs_100[[i]][pick_ind], 
+#'                          n_inits = 10, seed = j)
+#'     ADVI_fr_10_draws[[i]][[j]] <- 
+#'       Imp_Resam_WOR_ADVI(ADVI_fullrank_draw_100[[i]][pick_ind], 
+#'                          ADVI_fullrank_lrs_100[[i]][pick_ind], 
+#'                          n_inits = 10, seed = j)
+#'   }
+#' }
+#' proc.time() - t_0
+#' 
+#' save(file = "../results/ADVI_10_draws.RData",
+#'      list = c("ADVI_mf_10_draws", "ADVI_fr_10_draws"))
+#' 
+#' 
+#' 
+#' 
 
